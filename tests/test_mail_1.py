8b95d22c17aa582540cb107cdf785138a20c05a7
macbook <yorikyorik25@gmail.com>
2023-04-23 16:31:02 +0300

    first 5 les

---
+++ tests/test_mail_1.py
@@ -0,0 +1,12 @@
+
+"""
+всі тестові функції починаютсья з слова test...
+"""
+def test_sending_mail_1(set_up, some):
+    print('Письмо отправлено 1')
+
+"""
+set_up передаєму в функ як аргумент
+"""
+def test_sending_mail_2(set_up, some):
+    print('Письмо отправлено 2')
---
+++ tests/test_mail_2.py
@@ -0,0 +1,7 @@
+
+
+def test_sending_mail_3(set_up):
+    print('Письмо отправлено 3')
+
+def test_sending_mail_4(set_up):
+    print('Письмо отправлено 4')
